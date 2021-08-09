e59bf24397c21c0e18d155261906ec84a6b211ad
Kuldeep Singh <kuldeep@codenamaste.com>
2021-08-09 13:04:35 +0530

    Big O example
    Big O example with for loop

--- BigO.playground/Contents.swift
+++ BigO.playground/Contents.swift
@@ -1,3 +1,28 @@
 import UIKit
 
-var str = "Hello, playground"
+//Linear Time complexity
+//Mark:- O(n) example
+let fish = ["dory", "bruce", "marlin", "nemo"];
+let nemo = ["nemo"];
+let everyone = ["dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"];
+
+let large = Array.init(repeating: "nemo", count: 100000)
+
+func findNemo(_ fish:[String]) {
+  for i in 0..<fish.count {
+    if (fish[i] == "nemo") {
+      print("Found NEMO!");
+    }
+  }
+
+// modern-style for loop
+  /*
+     fish.forEach { fishName in
+       if (fishName == "nemo") {
+         print("Found NEMO!");
+       }
+     }
+ */
+}
+
+findNemo(everyone)
